33a56aeab28cd3f75913e5301ca927e449706509
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2024-10-22 17:26:20 +0900

    update

--- src/theme/common.ts
+++ src/theme/common.ts
@@ -4,3 +4,15 @@ export const categoryColors: { [key: string]: string } = {
   仕事: "lightcoral",
   試み: "lightyellow",
 };
+
+//  パターン1
+//   学習: "lightgreen",
+//   筋トレ: "lightblue",
+//   仕事: "lightcoral",
+//   試み: "lightyellow",
+
+// パターン2
+//   学習: "lightblue",
+//   筋トレ: "lightcoral",
+//   仕事: "lightgreen",
+//   試み: "lightyellow",
